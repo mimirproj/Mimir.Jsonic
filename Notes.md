b25a837d70695dbb6df0ad74f22930e58a8364e6
Mimir <mimirproj@protonmail.com>
2021-05-25 18:05:49 +0200

    V1 alpha of Saturn lib for Saturn support

--- Notes.md
+++ Notes.md
@@ -3,3 +3,4 @@ dotnet nuget locals all --clear
 dotnet pack ./src/Core/Mimir.Jsonic.fsproj -c Release
 dotnet pack ./src/Fable/Mimir.Jsonic.Fable.fsproj -c Release
 dotnet pack ./src/Net/Mimir.Jsonic.Net.fsproj -c Release
+dotnet pack ./src/Saturn/Mimir.Jsonic.Saturn.fsproj -c Release
---
+++ src/Saturn/Saturn.fs
@@ -0,0 +1,31 @@
+﻿module Mimir.Jsonic.Saturn
+
+open System.IO
+open System.Text
+open Microsoft.AspNetCore.Http
+open Microsoft.Net.Http.Headers
+open FSharp.Control.Tasks.V2
+open Giraffe
+
+
+open Mimir.Jsonic
+open Mimir.Jsonic.Net
+
+let encode (codec:Codec<'a>) (data:'a) : HttpHandler =
+    fun (_ : HttpFunc) (ctx : HttpContext) ->
+        ctx.SetContentType "application/json; charset=utf-8"
+        let jsonText = Codec.encodeToString false codec data
+        let jsonBytes = Encoding.UTF8.GetBytes jsonText
+        ctx.WriteBytesAsync jsonBytes
+
+let tryDecodeAsync (codec:Codec<'a>) (ctx : HttpContext) =
+    task {
+        use reader = new StreamReader(ctx.Request.Body, Encoding.UTF8)
+        let! bodyText = reader.ReadToEndAsync()
+
+        return Codec.decodeString codec bodyText
+    }
+
+let decodeError errorText next (ctx : HttpContext) =
+    ctx.SetStatusCode 400
+    text errorText next ctx
